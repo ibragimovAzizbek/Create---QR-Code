6515a6607e5b2c4a55341eede01271f939e8341a
Azizbek <ibragimovazizbek.0102@gmail.com>
2024-12-15 08:35:12 +0500

    Ready

---
+++ lib/constants/path/path.dart
@@ -0,0 +1,9 @@
+class Url {
+  static final Url _inherentce = Url._init();
+
+  static Url get inherentce => _inherentce;
+
+  Url._init();
+
+  String url = "https://youtu.be/chE5RIAujBc?si=U7F3rhZzGPo6YuwZ";
+}
